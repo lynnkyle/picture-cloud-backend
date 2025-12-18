39185f665efb9012ec1f592663e40663e70bb09c
LinZeyuan <903586678@qq.com>
2025-12-18 17:19:21 +0800

    1-commit

---
+++ src/main/java/org/example/picturecloudbackend/PictureCloudBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.picturecloudbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureCloudBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureCloudBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/picturecloudbackend/PictureCloudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.picturecloudbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureCloudBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
